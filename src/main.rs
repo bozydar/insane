f77d15818ce2ad33c77fc6e59de88512d0fb0d21
bozydar <bozysob@gmail.com>
2020-02-23 14:10:53 +1300

    Tests pass

--- src/main.rs
+++ src/main.rs
@@ -2,68 +2,111 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-use std::fs;
-use std::env;
 
 use pest::Parser;
-
-#[derive(Parser)]
-#[grammar = "csv.pest"]
-pub struct CSVParser;
+use pest::iterators::{Pair, Pairs};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let file_name = args.get(1).unwrap();
 
-    let unparsed_file = fs::read_to_string(&file_name).expect(format!("Can't read {}", file_name).as_str());
-    let file = CSVParser::parse(Rule::file, &unparsed_file)
+}
+
+#[derive(Parser)]
+#[grammar = "sane.pest"]
+pub struct SaneParser;
+
+#[derive(Debug, PartialEq)]
+struct LetIn {
+    pub var: String,
+    pub value: Box<Expr>,
+    pub in_part: Box<Expr>,
+}
+
+#[derive(Debug, PartialEq)]
+enum Const {
+    String(String),
+    Numeric(f64)
+}
+
+#[derive(Debug, PartialEq)]
+enum Expr {
+    LetIn(LetIn),
+    Const(Const),
+    Ident(String)
+}
+
+fn parse_const(pair: Pair<'_, Rule>) -> Result<Expr, &str> {
+    let pair = pair.into_inner().next().unwrap();
+    match pair.as_rule() {
+        Rule::string => Ok(Expr::Const(Const::String(pair.as_str().to_string()))),
+        Rule::number => {
+            let value = pair.as_str().parse::<f64>().unwrap();
+            Ok(Expr::Const(Const::Numeric(value)))
+        }
+        _ => Err("Unknown const type")
+    }
+}
+
+fn parse_let_in(pair: Pair<'_, Rule>) -> Result<Expr, &str> {
+    let mut inner: Pairs<'_, Rule> = pair.into_inner();
+    let ident = inner.next().unwrap();
+    let value = inner.next().unwrap();
+    let in_part = inner.next().unwrap();
+
+    Ok(Expr::LetIn(LetIn{
+        var: ident.as_str().to_string(),
+        value: Box::new(parse_pair(value)?),
+        in_part: Box::new(parse_pair(in_part)?)
+    }))
+}
+
+fn parse_ident(pair: Pair<'_, Rule>) -> Result<Expr, &str> {
+    Ok(Expr::Ident(pair.as_str().to_string()))
+}
+
+fn parse_pair(pair: Pair<'_, Rule>) -> Result<Expr, &str> {
+    let rule = pair.as_rule();
+    match rule {
+        Rule::constant => parse_const(pair),
+        Rule::let_in => parse_let_in(pair),
+        Rule::ident => parse_ident(pair),
+        _ => {
+            println!("--------------------Unknown rule {:?}", rule);
+            Err("Unknown rule")
+        }
+    }
+}
+
+fn parse_sane(input: &str) -> Result<Expr, &str> {
+    let parsed = SaneParser::parse(Rule::expr, input)
         .expect("Can't parse")
         .next()
         .unwrap();
 
-    let mut field_sum: f64 = 0.0;
-    let mut record_count: u64 = 0;
-    let mut strings: Vec<String> = vec![];
+    // let result: Option<pest::iterators::Pair<'_, Rule>> = parsed;
+    println!("-----------{:?}", parsed);
 
-    for record in file.into_inner() {
-        match record.as_rule() {
-            Rule::record => {
-                record_count += 1;
+    parse_pair(parsed)
+}
 
-                for field in record.into_inner() {
-                    match field.as_rule() {
-                        Rule::field => {
-                            for field_type in field.into_inner() {
-                                match field_type.as_rule() {
-                                    Rule::quoted_field => {
-                                        let inner = field_type.into_inner().nth(0).expect("Problem with the first element");
-                                        strings.push(inner.as_str().to_string());
-                                    }
-                                    Rule::unquoted_field => {
-                                        field_sum += field_type.as_str().parse::<f64>().unwrap();
-                                    }
-                                    _ => ()
-                                }
-                            }
-                        }
-                        _ => {
-                            ()
-                        }
-                    }
-                }
-            }
-            Rule::EOI => (),
-            _ => unreachable!(),
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_number() {
+        let result = parse_sane("-23.1");
+        assert_eq!(result, Ok(Expr::Const(Const::Numeric(-23.1))));
     }
 
-    println!("strings: {:?}", strings);
-    println!("Sum: {}", field_sum);
-    println!("Count: {}", record_count);
+    #[test]
+    fn test_parse_string() {
+        let result = parse_sane("\"test\"").unwrap();
+        assert_eq!(result, Expr::Const(Const::String("test".to_string())));
+    }
 
-    let successful_parse = CSVParser::parse(Rule::field, "\"-273.15\"");
-    println!("{:?}", successful_parse);
-
-    let unsuccessful_parse = CSVParser::parse(Rule::field, "this is not a number");
-    println!("{:?}", unsuccessful_parse);
+    #[test]
+    fn parse_let_in() {
+        let result = parse_sane("let a = 1 in a").unwrap();
+        assert_eq!(result, Expr::LetIn(LetIn { var: "a".to_string(), value: Box::new(Expr::Const(Const::Numeric(1.0))), in_part: Box::new(Expr::Ident("a".to_string())) }));
+    }
 }
